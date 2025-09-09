f5fe4425d2508199161affd34d5ea3e0857a287d
Abhinanda A M <abhiluckylad03@gmail.com>
2025-09-09 07:45:42 +0530

    Add files via upload

---
+++ src/project.java
@@ -0,0 +1,23 @@
+
+public class project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        final String College_name = "Sri sairam college of engineering";
+
+        String Student_name = "Abhinanda A M";
+        int Student_age = 22;
+        int Student_rollno = 112;
+        float Student_Fees = 45.6f;
+        char Student_grade = 'A';
+        System.out.println("===== Student Information Card =====");
+        System.out.println("Student Name : - " + Student_name);
+        System.out.println("Student age : - " + Student_age);
+        System.out.println("Student Roll Number : - "+ Student_rollno);
+        System.out.println("Student Fees : - " + Student_Fees);
+        System.out.println("Student Grade : - " + Student_grade);
+        System.out.println("College : - " + College_name);
+        
+	}
+
+}
